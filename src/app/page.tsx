d5d15df7291d512fd40f20dbdb2cdabd9be0722c
Victor Wu <vicwu.off@gmail.com>
2025-04-01 12:56:55 -0400

    adds welcome to front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./styles.module.css";
 
-import Image from "next/image";
+// import Image from "next/image";
 
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
@@ -13,32 +13,7 @@ export default function Home() {
 
       {/* Main */}
       <main className={styles.main}>
-        
-        {/* <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div> */}
+        <h1> Welcome! </h1>
       </main>
 
       {/* Footer Element */}
